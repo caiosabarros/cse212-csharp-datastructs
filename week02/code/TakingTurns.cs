d3c8343b6e87599afae9e9050968ff3197339e02
Caio Barros <caiohenriquesa1@gmail.com>
2024-03-08 14:39:59 -0300

    02: taking turns queue

--- week02/code/TakingTurns.cs
+++ week02/code/TakingTurns.cs
@@ -1,5 +1,7 @@
-﻿public static class TakingTurns {
-    public static void Test() {
+﻿public static class TakingTurns
+{
+    public static void Test()
+    {
         // TODO Problem 1 - Run test cases and fix the code to match requirements
         // Test Cases
 
@@ -12,11 +14,11 @@
         players.AddPerson("Bob", 2);
         players.AddPerson("Tim", 5);
         players.AddPerson("Sue", 3);
-        // Console.WriteLine(players);    // This can be un-commented out for debug help
+        Console.WriteLine(players);    // This can be un-commented out for debug help
         while (players.Length > 0)
             players.GetNextPerson();
         // Defect(s) Found: 
-
+        // the defect was that the enqueue function was inserting a Person at the beginning of the queue, whereas it should insert the person at the end, since Queue has a FIFO approach.
         Console.WriteLine("---------");
 
         // Test 2
@@ -28,7 +30,8 @@
         players.AddPerson("Bob", 2);
         players.AddPerson("Tim", 5);
         players.AddPerson("Sue", 3);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++)
+        {
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
@@ -38,7 +41,8 @@
         while (players.Length > 0)
             players.GetNextPerson();
 
-        // Defect(s) Found: 
+        // Defect(s) Found: Same problemas as with Test 1. Once that was solved, this one was solved as well.
+
 
         Console.WriteLine("---------");
 
@@ -52,15 +56,16 @@
         players.AddPerson("Tim", 0);
         players.AddPerson("Sue", 3);
         // Console.WriteLine(players);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
         // Defect(s) Found: 
-
+        // The getNextPerson() didn't have support to enqueue the person that had a Turns property of value 0.
         Console.WriteLine("---------");
 
-         // Test 4
+        // Test 4
         // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
         // Run 10 times.
         // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
@@ -69,11 +74,13 @@
         players.AddPerson("Tim", -3);
         players.AddPerson("Sue", 3);
         // Console.WriteLine(players);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
         // Defect(s) Found: 
+        // The getNextPerson() didn't have support to enqueue the person that had a Turns property of value less than 0.
 
         Console.WriteLine("---------");
 
@@ -84,5 +91,6 @@
         players = new TakingTurnsQueue();
         players.GetNextPerson();
         // Defect(s) Found:
+        // Fixing the above test cases fixed this one as well.
     }
 }
